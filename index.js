7ab27646c8b2499e8d26ecf21392f9b1f0d5a407
Antonio <manentea@gmail.com>
2015-07-17 18:21:06 -0400

    added display name feature for chat

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
   $('.chat-box').on('keyup', function(event){
+    var name = $('.display').val()
     if(event.keyCode == 13){
-      $('.chat').append("<div class='message'>" + this.value + '</div>');
+      $('.chat').append("<div class='message'>" + name + ': ' +  this.value + '</div>');
       this.value = '';
       $(".chat").animate({ scrollTop: $(document).height() }, "slow");
     }
